1a8ba6c04e2b2839bc34df0c5246b3829330c262
mindexpert7546 <kk7615117@gmail.com>
2024-06-11 22:53:08 +0530

    Save current changes before updating Angular

--- clientCode/src/app/app.module.ts
+++ clientCode/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { DropdownModule } from 'primeng/dropdown';
+import { TagModule } from 'primeng/tag';
+import { FormsModule } from '@angular/forms';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,7 +20,15 @@ import { HttpClientModule } from '@angular/common/http'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    TableModule,
+    ToastModule,
+    DropdownModule,
+    TagModule,
+    FormsModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
